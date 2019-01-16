a85de001e4859918360c7dfc1b7ca5537decfb4b
student <bettyjcawthon@gmail.com>
2019-01-16 16:39:08 -0600

    map/this aft cw

---
+++ Filter2.js
@@ -0,0 +1,35 @@
+//Use .filter to create an array of pet objects that only holds the hamsters.
+// let pets = [
+// {
+//   name: 'Spot',
+//   type: 'Hamster'
+// },
+// {
+//   name: 'Harry',
+//   type: 'Hamster'
+// },
+// {
+//   name: 'Kitty',
+//   type: 'Cat'
+// }
+// ]
+//
+// let hamsters = []; // new array to hold objects
+
+
+var pets =[{"Spot": "Hamster"}, {"Harry": "Hamster"}, {"Kitty": "Cat"}];
+
+function checkPetType (pets)
+{
+    return animal.type === "hamster"
+}
+
+function checkPetType ()
+{
+    let hamsters =pets.filter.function(checkPetType)
+    {
+
+        return("hamsters")
+    }
+
+}
---
+++ Map1.js
@@ -0,0 +1,40 @@
+//Use .map to create a new array of objects where each object has a property for petnumber and pettype based on the
+// elements in the array. Print the new array of objects.
+// let arr = ["dog", "cat", "pig", "hamster"];
+// let pet_objects = [];,  // new array to hold objects
+
+var arr = ["dog", "cat", "pig", "hamster"];
+var objArray = arr.map(function())
+{
+
+    let newObj = [];
+
+    newObj.petnumber = 45;
+    newObj.pettype = "meatEater";
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
